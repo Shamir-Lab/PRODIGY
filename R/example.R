c25f13d20764e9479b9196476577b0ffbef94e48
galdinstag <galdinstag@gmail.com>
2022-02-11 12:07:59 +0200

    Edit example

--- R/example.R
+++ R/example.R
@@ -16,9 +16,10 @@
 #' # Identify sample origins (tumor or normal)
 #' sample_origins = rep("tumor",ncol(expression_matrix))
 #' sample_origins[substr(colnames(expression_matrix),nchar(colnames(expression_matrix)[1])-1,nchar(colnames(expression_matrix)[1]))=="11"] = "normal"	
+#' list_of_pathways = get_pathway_list_from_graphite(source = "reactome",minimal_number_of_nodes = 10,num_of_cores = 20)
 #' # Run PRODIGY
 #' all_patients_scores = PRODIGY_cohort(snv_matrix,expression_matrix,network=network,samples=samples,DEGs=DEGs,alpha=0.05,
-#' 			pathwayDB="reactome",num_of_cores=1,sample_origins=sample_origins,write_results = F, results_folder = "./",
+#' 			pathway_list=list_of_pathways,num_of_cores=1,sample_origins=sample_origins,write_results = F, results_folder = "./",
 #' 			beta=2,gama=0.05,delta=0.05)
 #' # Get driver gene rankings for all samples 
 #' results = analyze_PRODIGY_results(all_patients_scores)
